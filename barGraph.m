716960ffb49704cc8e2e60e4b3d075bc79c67af8
maryk <maryk@DESKTOP-9QJF23Q.dhcp.nd.edu>
2021-05-05 16:52:26 -0400

    lots of stuff

--- barGraph.m
+++ barGraph.m
@@ -1,42 +1,49 @@
-%% bar graph 
-
-% find averages for each demographic  
-
-
-% Europe and Central Asia row 235 
-
-ECA=cell2mat(edudata(235,7:end))
-ECAtot=0
-for i=1:length(ECA) 
-    ECAtot=ECA(i)+ECAtot
-end 
-ECAav=ECAtot/length(ECA) 
+function barGraph(app)
+myRow=findCountry(app.countryDropDown.Value);
+[x,y]= arrayMaker(myRow);
+bar(app.barAxes,x,y);
+end
 
 
-% sub Sahara Africa row 244 
-
-SSA=cell2mat(edudata(244,7:end))
-SSAtot=0
-for i=1:length(SSA) 
-    SSAtot=SSA(i)+SSAtot
-end 
-SSAav=SSAtot/length(SSA) 
-
-% Latin America and Carribean row 238 
-
-LAC=cell2mat(edudata(238,7:end))
-LACtot=0
-for i=1:length(LAC) 
-    LACtot=SSA(i)+LACtot
-end 
-LACav=LACtot/length(LAC ) 
-
-
-labels=['Europe and Central Asia','Sub Sahara Africa','Latin America and Carribean'] 
-
-counts=[ECAav,SSAav,LACav] 
-
-bar(app.barAxes,counts) 
-set(gca,'xticklabel',labels) 
+% %% bar graph 
+% 
+% % find averages for each demographic  
+% 
+% 
+% % Europe and Central Asia row 235 
+% 
+% ECA=cell2mat(edudata(235,7:end))
+% ECAtot=0
+% for i=1:length(ECA) 
+%     ECAtot=ECA(i)+ECAtot
+% end 
+% ECAav=ECAtot/length(ECA) 
+% 
+% 
+% % sub Sahara Africa row 244 
+% 
+% SSA=cell2mat(edudata(244,7:end))
+% SSAtot=0
+% for i=1:length(SSA) 
+%     SSAtot=SSA(i)+SSAtot
+% end 
+% SSAav=SSAtot/length(SSA) 
+% 
+% % Latin America and Carribean row 238 
+% 
+% LAC=cell2mat(edudata(238,7:end))
+% LACtot=0
+% for i=1:length(LAC) 
+%     LACtot=SSA(i)+LACtot
+% end 
+% LACav=LACtot/length(LAC ) 
+% 
+% 
+% labels=['Europe and Central Asia','Sub Sahara Africa','Latin America and Carribean'] 
+% 
+% counts=[ECAav,SSAav,LACav] 
+% 
+% bar(app.barAxes,counts) 
+% set(gca,'xticklabel',labels) 
 
 
--- lineGraph.m
+++ lineGraph.m
@@ -1,17 +1,17 @@
 function lineGraph(app)
-table = table2array(readtable('test123.xlsx'));
-[row,col]=size(table);
 myRow=findCountry(app.secondCountry.Value);
-x=[];
-y=[];
-counter=1;
-for iCnt=2:col
-    if table(myRow,iCnt)~= 0
-        x(counter)= table(1,iCnt);
-        y(counter)= table(myRow,iCnt);
-        counter=counter+1;
-    end
-end
+[x,y]= arrayMaker(myRow);
+% myRow=findCountry(app.secondCountry.Value);
+% x=[];
+% y=[];
+% counter=1;
+% for iCnt=2:col
+%     if table(myRow,iCnt)~= 0
+%         x(counter)= table(1,iCnt);
+%         y(counter)= table(myRow,iCnt);
+%         counter=counter+1;
+%     end
+% end
 if size(x)==0
     plot(app.plotAxes,0,0);
 else
