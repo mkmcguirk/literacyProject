f0c463b4df3d29795bc22e2e7095206ccc1c2085
maryk <maryk@DESKTOP-9QJF23Q.dhcp.nd.edu>
2021-05-06 13:59:53 -0400

    worked on the map and such, fun stuff!

--- secondLineGraph.m
+++ secondLineGraph.m
@@ -16,16 +16,16 @@ secondCountry=findCountry(app.secondCountry.Value);
 if size(a)==0 & size(c)==0
      plot(app.plotAxes,0,0);
 elseif size (a)==0 & size(c)~=0
-     plot(app.plotAxes,c,d,'o-');  
+     plot(app.plotAxes,c,d,'m-');  
 elseif size (a)~=0 & size(c)==0
-     plot(app.plotAxes,a,b,'*-');
+     plot(app.plotAxes,a,b,'b-');
 elseif size (a)~=0 & size(c)~=0
-        plot(app.plotAxes,a,b,'*-',...
-            c,d,'o-');
+        plot(app.plotAxes,a,b,'b-',...
+            c,d,'m-');
 end  
         
 xlabel(app.plotAxes,'Year');
 ylabel(app.plotAxes,'Literacy Rate %');
 title(app.plotAxes,['Data for ',app.countryDropDown.Value, ' and ', app.secondCountry.Value]);
-
+legend(app.plotAxes,app.countryDropDown.Value, app.secondCountry.Value, 'Location', 'Northwest')
 end
